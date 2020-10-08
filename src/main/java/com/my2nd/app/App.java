73246112c99bf006434c115437e9ce1359d69809
Dheerendra Kumar Singh <dheerendra@gmail.com>
2020-10-08 11:18:31 +0530

    commit message

---
+++ src/main/java/com/my2nd/app/App.java
@@ -0,0 +1,13 @@
+package com.my2nd.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
